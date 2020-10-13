6f7476f74a725fc4212ec3340eb0c634bb293511
Nosikov <nosevg@yandex.ru>
2020-10-13 20:22:58 +0700

    urok po cmd

---
+++ myfoldy/create.cmd
@@ -0,0 +1,17 @@
+@echo OFF
+if not exist main (
+echo "Create my system"
+mkdir main
+mkdir main\001
+mkdir main\002
+mkdir main\001\a
+mkdir main\001\b
+mkdir main\002\c
+mkdir main\002\d
+) else (
+   echo "Folder main already exist"
+   )
+echo ON
+
+
+
